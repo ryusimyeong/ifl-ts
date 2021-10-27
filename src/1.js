408f0d177d2eb17a56b4d1a6a71da0b123408e54
simmy <simmy@freshcode.me>
2021-10-27 16:37:14 +0900

    타입스크립트 실행방법 부터 몇 가지 팁

--- src/1.js
+++ src/1.js
@@ -1,3 +1,6 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const lodash_1 = require("lodash");
 const v1 = 123;
 const v2 = () => console.log('123');
+lodash_1.isEqual;
--- src/1.ts
+++ src/1.ts
@@ -1,2 +1,6 @@
+import { isEqual } from "lodash";
+
 const v1: number | string = 123;
 const v2 = () => console.log('123');
+
+isEqual
