feea4cc20693b2d7023f8b90f7f3d0b40d9e0822
simmy <simmy@freshcode.me>
2021-10-29 18:05:54 +0900

    함수타입2

---
+++ src/5.ts
@@ -0,0 +1,5 @@
+function getText(name: string, age = 15, language?: string): string {
+  const nameText = name.substr(0, 10);
+  const ageText = age >= 35 ? 'senior' : 'junior';
+  return `name: ${nameText}, age: ${ageText}, language: ${language}`;
+}
